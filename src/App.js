dda8ca4e152f13370c43d483b02aef96b288f269
Thanachit Sengsalee <47846297+6010110455@users.noreply.github.com>
2022-04-14 18:59:36 +0700

    วางโครงทั้งหมด

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="text-4xl">HELLO</div>;
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
