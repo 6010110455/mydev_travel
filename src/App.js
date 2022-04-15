60470cc58f508fa95c073eafc1af6d2910af59b9
Thanachit Sengsalee <47846297+6010110455@users.noreply.github.com>
2022-04-15 20:19:17 +0700

    เพิ่มการเเสดงลิสรูปภาพ ล่าง Hero

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
 
 function App() {
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <Destinations />
     </div>
   );
 }
